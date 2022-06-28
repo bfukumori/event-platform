d908796528a453a19face16b53809e499cfe23e7
Bruno Fukumori <brunofukumori@gmail.com>
2022-06-28 15:50:57 -0300

    layout responsive to subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -26,21 +26,23 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+      <div className="w-full max-w-[1100px] flex flex-col md:flex-row justify-between mt-10 md:mt-20 mx-auto">
+        <div className="md:max-w-[640px] px-6">
+          <div className="flex justify-center md:justify-start">
+            <Logo />
+          </div>
+          <h1 className="mt-6 text-[2.5rem] leading-tight text-center md:text-left">
             Construa uma{" "}
             <strong className="text-blue-500">aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-6 mb-8 w-xs text-center md:text-left text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded md:mx-6">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
